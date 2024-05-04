cbce0b7a3014360198c7a2272c1a58e554c3d910
David Meaux <david@davidmeaux.com>
2024-05-04 17:19:14 +0200

    flake8 error fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -8,33 +7,34 @@ class CarMake(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return self.name # Return the name as the string representation
+        return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('HATCHBACK', 'Hatchback'),
-        ('COUPE', 'Coupe'),
-        ('MINIVAN', 'Minivan'),
-        ('CONVERTIBLE', 'Convertible'),
-        ('PICKUP', 'Pickup'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        ("HATCHBACK", "Hatchback"),
+        ("COUPE", "Coupe"),
+        ("MINIVAN", "Minivan"),
+        ("CONVERTIBLE", "Convertible"),
+        ("PICKUP", "Pickup"),
     ]
-    type = models.CharField(max_length=11, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+    type = models.CharField(max_length=11, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
     dealer_id = models.IntegerField()
-    mileage = models.IntegerField(default=0,
+    mileage = models.IntegerField(
+        default=0,
         validators=[
             MinValueValidator(0),
-        ]
+        ],
     )
 
     def __str__(self):
